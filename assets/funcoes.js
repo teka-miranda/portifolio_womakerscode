ce9ef8433ae5d280ef1dd85601452a4bc1c6b660
Leticia Miranda de Souzza <134153140+teka-miranda@users.noreply.github.com>
2023-11-13 16:11:55 -0300

    Add files via upload

---
+++ assets/funcoes.js
@@ -0,0 +1,7 @@
+/* Criar um protifolio, descrevendo cursos, ou atividades anteriores realizadas para desenvolvimento de site.
+passo 1 - criar portifolio descrevendo sobre voce
+passo 2 - listar cursos realizados na área de desenvolvedor
+passo 3 - lincar sites pessoais, instagram, twitter, linkdin
+
+*/
+
